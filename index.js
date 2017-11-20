3701c465ebb3fc5d53326b6e147bb6bcbf5c0a86
Fly J Gore <jennifer@theothergore.com>
2017-11-20 17:10:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function scuberGreetingForFeet(distance) {
   if (distance <= 400){
     return "This one is on me!";
+  } else if (distance > 2000){
+    return "I will gladly take your thirty bucks."
   }
 }
